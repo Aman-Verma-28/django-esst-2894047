5975516ab76b32feb9f2c3456037e664eaa40588
Aman-Verma-28 <vermaaman2809@gmail.com>
2022-06-21 16:34:40 +0530

    changing the admin file

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Notes
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+admin.site.register(Notes, NotesAdmin)
